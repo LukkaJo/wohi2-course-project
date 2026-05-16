dd032668fccdfa951e28f1872a6c53c7f7cf5861
LukkaJo <jonatanlukkarinen26@gmail.com>
2026-05-16 13:54:42 +0300

    Fix migrations: merge likes into attempts

--- prisma/migrations/20260507182700_add_attempts/migration.sql
+++ prisma/migrations/20260507182700_add_attempts/migration.sql
@@ -1,3 +1,20 @@
+-- DropForeignKey
+ALTER TABLE `posts` DROP FOREIGN KEY `posts_userId_fkey`;
+-- DropIndex
+DROP INDEX `posts_userId_fkey` ON `posts`;
+-- DropTable
+DROP TABLE `user`;
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- AddForeignKey
+ALTER TABLE `posts` ADD CONSTRAINT `posts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 -- CreateTable
 CREATE TABLE `Attempt` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
@@ -6,12 +23,9 @@ CREATE TABLE `Attempt` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `userId` INTEGER NOT NULL,
     `postId` INTEGER NOT NULL,
-
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 -- AddForeignKey
 ALTER TABLE `Attempt` ADD CONSTRAINT `Attempt_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
 -- AddForeignKey
 ALTER TABLE `Attempt` ADD CONSTRAINT `Attempt_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `posts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
